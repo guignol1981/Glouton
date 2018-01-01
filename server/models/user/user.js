3bef62f55a004c33ecdf5cc3120e4b34d2c417b1
vguillemette <vguillemette@devalto.com>
2018-01-01 17:35:42 -0500

    heroku

--- server/models/user/user.js
+++ server/models/user/user.js
@@ -23,11 +23,11 @@ let userSchema = new Schema({
 
 userSchema.methods.setPassword = function (password) {
 	this.salt = crypto.randomBytes(16).toString('hex');
-	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 };
 
 userSchema.methods.validPassword = function (password) {
-	let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 	return this.hash === hash;
 };
 
